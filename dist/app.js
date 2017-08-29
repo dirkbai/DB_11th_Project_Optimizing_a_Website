541ec72fb8a77f045c0dd45842aa1587b0582bea
Dirk B <dirk.baigent@example.com>
2017-08-29 06:45:47 +0200

    npm test run and Styling the Logo

--- dist/app.js
+++ dist/app.js
@@ -1400,12 +1400,69 @@ var _extends=Object.assign||function(a){for(var b=1;b<arguments.length;b++){var 
 
 console.log('scrpts!!!!+++');
 
+var lazyModals = document.getElementsByClassName('mod');
 var photo4 = document.getElementById('photo4');
 var photo5 = document.getElementById('photo5');
 var photo6 = document.getElementById('photo6');
 var photo7 = document.getElementById('photo7');
 var photo8 = document.getElementById('photo8');
 var photo9 = document.getElementById('photo9');
+
+
+function lazyModal_1() {
+      lazyModals[0].classList.add('loaded');
+      lazyModals[0].setAttribute('src', 'img/XL/photo1.jpg');
+      lazyModals[0].setAttribute('data-was-processed', "true");
+    }
+
+function lazyModal_2() {
+      lazyModals[1].classList.add('loaded');
+      lazyModals[1].setAttribute('src', 'img/XL/photo2.jpg');
+      lazyModals[1].setAttribute('data-was-processed', "true");
+    }
+
+function lazyModal_3() {
+      lazyModals[2].classList.add('loaded');
+      lazyModals[2].setAttribute('src', 'img/XL/photo3.jpg');
+      lazyModals[2].setAttribute('data-was-processed', "true");
+    }
+
+function lazyModal_4() {
+      lazyModals[3].classList.add('loaded');
+      lazyModals[3].setAttribute('src', 'img/XL/photo4.jpg');
+      lazyModals[3].setAttribute('data-was-processed', "true");
+    }
+
+function lazyModal_5() {
+      lazyModals[4].classList.add('loaded');
+      lazyModals[4].setAttribute('src', 'img/XL/photo5.jpg');
+      lazyModals[4].setAttribute('data-was-processed', "true");
+    }
+
+function lazyModal_6() {
+      lazyModals[5].classList.add('loaded');
+      lazyModals[5].setAttribute('src', 'img/XL/photo6.jpg');
+      lazyModals[5].setAttribute('data-was-processed', "true");
+    }
+
+function lazyModal_7() {
+      lazyModals[6].classList.add('loaded');
+      lazyModals[6].setAttribute('src', 'img/XL/photo7.jpg');
+      lazyModals[6].setAttribute('data-was-processed', "true");
+    }
+
+function lazyModal_8() {
+      lazyModals[7].classList.add('loaded');
+      lazyModals[7].setAttribute('src', 'img/XL/photo8.jpg');
+      lazyModals[7].setAttribute('data-was-processed', "true");
+    }
+
+function lazyModal_9() {
+      lazyModals[8].classList.add('loaded');
+      lazyModals[8].setAttribute('src', 'img/XL/photo9.jpg');
+      lazyModals[8].setAttribute('data-was-processed', "true");
+    }
+
 
 document.addEventListener('scroll', function() {
   photo4.setAttribute('src', 'img/photo4.jpg');
